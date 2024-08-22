42df977d03f24a0c247127a656dafe38f80ccbcb
Teodor Englund <24545202+Hyltcavl@users.noreply.github.com>
2024-08-22 12:45:34 +0000

    now creates a list of callable links to all available cards

--- mtg-rust/src/dl/list_links.rs
+++ mtg-rust/src/dl/list_links.rs
@@ -1,7 +1,21 @@
-use regex::Regex;
 use reqwest::blocking;
 use scraper::{Html, Selector};
-use std::{any::Any, error::Error, fmt::format};
+
+pub fn get_page_count(url: &str) -> Option<u32> {
+    let response = blocking::get(url).ok()?;
+    let html_content = response.text().ok()?;
+
+    let parse_document = Html::parse_document(&html_content);
+    let table_selector = Selector::parse("div.container.align-center.pagination a").ok()?;
+    let selected_elements: Vec<_> = parse_document.select(&table_selector).collect();
+
+    if selected_elements.len() >= 3 {
+        let last_page_link = &selected_elements[selected_elements.len() - 3];
+        last_page_link.inner_html().parse::<u32>().ok()
+    } else {
+        Some(1) // Assuming there's at least one page if no pagination is found
+    }
+}
 
 pub fn request_page(cmc: u32, page_count: u32) -> Option<String> {
     let request_url = format_args!(
@@ -11,14 +25,21 @@ pub fn request_page(cmc: u32, page_count: u32) -> Option<String> {
     .to_string();
 
     // i need a way to try and get the text, and if i fail, return none, otherways return some witht he String in it.
-    let result = match blocking::get(request_url) {
+    let result = match blocking::get(&request_url) {
         Ok(response) => response.text(),
         Err(e) => {
-            println!("Failed to fetch URL: {} \n with error {}", request_url, e);
+            println!("Failed to fetch URL: {} \n with error: {}", &request_url, e);
             return None;
         }
     };
-    result
+
+    match result {
+        Ok(result) => Some(result),
+        Err(e) => {
+            println!("Failed get response text with error: {}", e);
+            return None;
+        }
+    }
 }
 
 #[cfg(test)]
@@ -30,29 +51,59 @@ mod tests {
 
     #[test]
     fn test_fetch_and_parse() {
-        request_page()
-        // let html_content =
-        //     fs::read_to_string("/workspaces/mtg-prz-rust/product_search_page.html").unwrap();
+        let res = request_page(0, 0);
+        println!("{}", res.unwrap())
+    }
 
-        // let mut server = mockito::Server::new();
-        // let url = server.url();
-        // // Create a mock
-        // let mock = server
-        //     .mock("GET", "/product/card-singles/magic?name=reaper+king")
-        //     .with_status(200)
-        //     .with_header("content-type", "text/plain")
-        //     .with_header("x-api-key", "1234")
-        //     .with_body(html_content.clone())
-        //     .create();
+    #[test]
+    fn test_get_page_count() {
+        let html_content =
+            fs::read_to_string("/workspaces/mtg-prz-rust/get_pages_page.html").unwrap();
+        let mut server = mockito::Server::new();
+        let url = server.url();
+        let mock = server
+            .mock(
+                "GET",
+                "/product/magic/card-singles/store:kungsholmstorg/cmc-0/1",
+            )
+            .with_status(200)
+            .with_header("content-type", "text/plain")
+            .with_header("x-api-key", "1234")
+            .with_body(html_content.clone())
+            .create();
+        let url = format!(
+            "{}/product/magic/card-singles/store:kungsholmstorg/cmc-0/1",
+            url
+        );
 
-        // // let url = &mockito::server_url();
-        // let result = fetch_and_parse(&format!(
-        //     "{}/product/card-singles/magic?name=reaper+king",
-        //     url
-        // ))
-        // .unwrap();
+        let res = get_page_count(&url);
+        mock.assert();
+        assert_eq!(res.unwrap(), 51);
+    }
 
-        // mock.assert();
-        // assert_eq!(result.len(), 8);
+    #[test]
+    fn test_get_page_count_no_pages() {
+        let html_content =
+            fs::read_to_string("/workspaces/mtg-prz-rust/get_pages_no_pages.html").unwrap();
+        let mut server = mockito::Server::new();
+        let url = server.url();
+        let mock = server
+            .mock(
+                "GET",
+                "/product/magic/card-singles/store:kungsholmstorg/cmc-15/1",
+            )
+            .with_status(200)
+            .with_header("content-type", "text/plain")
+            .with_header("x-api-key", "1234")
+            .with_body(html_content.clone())
+            .create();
+        let url = format!(
+            "{}/product/magic/card-singles/store:kungsholmstorg/cmc-15/1",
+            url
+        );
+
+        let res = get_page_count(&url);
+        mock.assert();
+        assert_eq!(res.unwrap(), 1);
     }
 }
--- mtg-rust/src/main.rs
+++ mtg-rust/src/main.rs
@@ -1,15 +1,37 @@
 // main.rs
 mod dl;
-use dl::card_parser::fetch_and_parse;
+use dl::{card_parser::fetch_and_parse, list_links::get_page_count};
 
 fn main() {
-    let url = "https://astraeus.dragonslair.se/product/card-singles/magic?name=reaper+king";
-    match fetch_and_parse(url) {
-        Ok(results) => {
-            for result in results {
-                println!("{:#?}", result);
-            }
-        }
-        Err(e) => eprintln!("Error: {}", e),
+
+    let mut card_urls = Vec::new();
+    for x in [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 15, 16] {
+        let request_url = format_args!(
+            "https://astraeus.dragonslair.se/product/magic/card-singles/store:kungsholmstorg/cmc-{}/{}",
+            x, 0,
+        )
+        .to_string();
+        let page_count = get_page_count(&request_url).unwrap_or(1);
+
+        let pages_to_call = (1 .. page_count+1).collect::<Vec<u32>>()
+        .into_iter()
+        .map(|count| format_args!(
+                "https://astraeus.dragonslair.se/product/magic/card-singles/store:kungsholmstorg/cmc-{}/{}",
+                x, count,
+            ).to_string()
+         )
+         .collect::<Vec<String>>();
+        card_urls.push(pages_to_call);
     }
+
+    // println!("{:?}", card_urls);
+    // let url = "https://astraeus.dragonslair.se/product/card-singles/magic?name=reaper+king";
+    // match fetch_and_parse(url) {
+    //     Ok(results) => {
+    //         for result in results {
+    //             println!("{:#?}", result);
+    //         }
+    //     }
+    //     Err(e) => eprintln!("Error: {}", e),
+    // }
 }
