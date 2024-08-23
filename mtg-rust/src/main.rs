e3dcc399988de2c0d142b37cd588289c80fde68a
Teodor Englund <24545202+Hyltcavl@users.noreply.github.com>
2024-08-23 09:26:37 +0000

    calling in paralell

--- mtg-rust/src/main.rs
+++ mtg-rust/src/main.rs
@@ -1,38 +1,48 @@
 // main.rs
 mod dl;
-use dl::{card_parser::fetch_and_parse, list_links::get_page_count};
+use std::sync::Arc;
+
+use dl::list_links::get_page_count;
+use futures::future::join_all;
+use tokio::sync::Semaphore;
+use tokio::try_join;
 
 #[tokio::main]
 async fn main() {
-    let mut card_urls = Vec::new();
-    for x in [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 15, 16] {
-        let request_url = format_args!(
-            "https://astraeus.dragonslair.se/product/magic/card-singles/store:kungsholmstorg/cmc-{}/{}",
-            x, 0,
-        )
-        .to_string();
-        let page_count = get_page_count(&request_url).await.unwrap();
-        println!("x: {}, and page_count is{:?}", x, page_count);
+    let base_url = "https://astraeus.dragonslair.se".to_owned();
+    let semaphore = Arc::new(Semaphore::new(10));
+    let cmcs_available = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 15, 16];
 
-        let pages_to_call = (1 .. page_count+1).collect::<Vec<u32>>()
-        .into_iter()
-        .map(|count| format_args!(
-                "https://astraeus.dragonslair.se/product/magic/card-singles/store:kungsholmstorg/cmc-{}/{}",
-                x, count,
-            ).to_string()
-         )
-         .collect::<Vec<String>>();
-        card_urls.push(pages_to_call);
+    let page_fetches_asyncs = cmcs_available.into_iter().map(|cmc| {
+        let semaphore_clone = Arc::clone(&semaphore);
+        
+        tokio::spawn({
+            let value = base_url.clone();
+            async move {
+                let _permit = semaphore_clone.acquire().await.unwrap(); //Get a permit to run in parallel
+                let request_url = format_args!(
+                    "{}/product/magic/card-singles/store:kungsholmstorg/cmc-{}/{}",
+                    &value, cmc, 0
+                )
+                .to_string();
+                let page_count = get_page_count(&request_url).await.unwrap_or(1);
+                println!("cmc: {}, and page_count is {:?}", cmc, page_count);
+                (1..=page_count)
+                    .map(|count| {
+                        format!(
+                            "{}/product/magic/card-singles/store:kungsholmstorg/cmc-{}/{}",
+                            &value, cmc, count
+                        )
+                    })
+                    .collect::<Vec<String>>()
+            }
+        })
+    });
+    // Awaits all of the calls. Can only be done in a for loop, not in a .map
+    let mut results = Vec::new();
+    for handle in page_fetches_asyncs {
+        results.push(handle.await.unwrap());
     }
+    println!("{:?}", results);
 
-    // println!("{:?}", card_urls);
-    // let url = "https://astraeus.dragonslair.se/product/card-singles/magic?name=reaper+king";
-    // match fetch_and_parse(url) {
-    //     Ok(results) => {
-    //         for result in results {
-    //             println!("{:#?}", result);
-    //         }
-    //     }
-    //     Err(e) => eprintln!("Error: {}", e),
-    // }
 }
